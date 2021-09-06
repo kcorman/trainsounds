8f27187c60ab22ee44090b8b673bcc960bfaa21d
foo@bar.com <foo@bar.com>
2021-09-07 00:35:28 +0100

    Debugging

--- run_forever.sh
+++ run_forever.sh
@@ -1,7 +1,8 @@
+echo "Current user is `whoami`"
 while :
 do
 	echo "Running main python script"
-	python3 /home/pi/stuff/trainchug.py > /dev/null
+	python3 /home/pi/stuff/trainchug.py
 	echo "Script stopped. Restarting it..."
 	sleep 5
 done
